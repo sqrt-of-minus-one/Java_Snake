164f14b3fbed398915a83b8ecc7b5180a9dd2680
Evgeny Beryozoff <jenyaiu90@gmail.com>
2025-01-20 06:44:47 +0300

    Move selection
    Добавлено управление: теперь можно передвигать выделение на поле и выбирать ту или иную клетку.

--- src/main/java/edu/mephi/java/three/Field.java
+++ src/main/java/edu/mephi/java/three/Field.java
@@ -1,6 +1,7 @@
 package edu.mephi.java.three;
 
 import edu.mephi.java.engine.AbstractField;
+import edu.mephi.java.engine.EDirection;
 import edu.mephi.java.three.command.Command;
 import edu.mephi.java.three.tiles.Empty;
 import edu.mephi.java.three.tiles.Tile;
@@ -11,6 +12,7 @@ public class Field
 	private int selectedX = 0;
 	private int selectedY = 0;
 	private boolean picked = false;
+	private int score = 0;
 	
 	public Field(ThreeGame game)
 	{
@@ -36,6 +38,60 @@ public class Field
 	public boolean isPicked()
 	{
 		return picked;
+	}
+	
+	public int getScore()
+	{
+		return score;
+	}
+	
+	public void move(EDirection direction)
+	{
+		if (!getGame().isGameOver() && !getGame().isPaused())
+		{
+			switch (direction)
+			{
+				case EDirection.UP ->
+				{
+					if (--selectedY < 0)
+					{
+						selectedY = getSizeY() - 1;
+					}
+				}
+				case EDirection.DOWN ->
+				{
+					if (++selectedY >= getSizeY())
+					{
+						selectedY = 0;
+					}
+				}
+				case EDirection.LEFT ->
+				{
+					if (--selectedX < 0)
+					{
+						selectedX = getSizeX() - 1;
+					}
+				}
+				case EDirection.RIGHT ->
+				{
+					if (++selectedX >= getSizeX())
+					{
+						selectedX = 0;
+					}
+				}
+			}
+			getGame().updateField();
+		}
+	}
+	
+	public void pick()
+	{
+		if (!getGame().isGameOver() && !getGame().isPaused())
+		{
+			picked = !picked;
+			getGame().updateFieldSprite(selectedX, selectedY);
+			getGame().repaint();
+		}
 	}
 	
 	public void fillEmpty()
--- src/main/java/edu/mephi/java/three/ThreeGame.java
+++ src/main/java/edu/mephi/java/three/ThreeGame.java
@@ -1,6 +1,7 @@
 package edu.mephi.java.three;
 
 import edu.mephi.java.engine.AbstractGame;
+import edu.mephi.java.engine.EDirection;
 import edu.mephi.java.three.command.Command;
 import edu.mephi.java.three.tiles.Tile;
 
@@ -43,13 +44,25 @@ public class ThreeGame
 	@Override
 	public void updateStatus()
 	{
-	
+		drawNumber(getStatusLabels(EStatusLabels.SCORE.getIndex()),
+				   1, getStatusLabels(EStatusLabels.SCORE.getIndex()).length,
+				   getField() != null ? getField().getScore() : 0);
 	}
 	
 	@Override
 	protected void controls(KeyEvent event)
 	{
-	
+		if (!isGameOver() && !isPaused())
+		{
+			switch (event.getKeyCode())
+			{
+				case KeyEvent.VK_UP, KeyEvent.VK_W -> getField().move(EDirection.UP);
+				case KeyEvent.VK_DOWN, KeyEvent.VK_S -> getField().move(EDirection.DOWN);
+				case KeyEvent.VK_LEFT, KeyEvent.VK_A -> getField().move(EDirection.LEFT);
+				case KeyEvent.VK_RIGHT, KeyEvent.VK_D -> getField().move(EDirection.RIGHT);
+				case KeyEvent.VK_ENTER, KeyEvent.VK_SPACE -> getField().pick();
+			}
+		}
 	}
 	
 	@Override
